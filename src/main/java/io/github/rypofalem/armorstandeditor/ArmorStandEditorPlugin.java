abb7dd630a474e4a10ca2dc11c588b2a0e83637d
RypoFalem <rypofalem@gmail.com>
2020-07-08 14:00:24 -0500

    fix disabled slot items not recognised as icons

--- src/main/java/io/github/rypofalem/armorstandeditor/ArmorStandEditorPlugin.java
+++ src/main/java/io/github/rypofalem/armorstandeditor/ArmorStandEditorPlugin.java
@@ -29,7 +29,6 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
-import java.util.logging.Level;
 
 public class ArmorStandEditorPlugin extends JavaPlugin{
 	private NamespacedKey iconKey;
