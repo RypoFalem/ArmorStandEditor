26afdefead45d194a94b79dd3868f6e4b2acd35f
Wolfstorm <xmakeitrain94@gmail.com>
2021-07-31 12:27:07 +0200

    [CORE] Fix Reload Command Permission checks
    Signed-off-by: Github <xMakeItRain94@gmail.com>
    Signed-off-by: Wolfstorm <xMakeItRain94@gmail.com>

--- src/main/java/io/github/rypofalem/armorstandeditor/CommandEx.java
+++ src/main/java/io/github/rypofalem/armorstandeditor/CommandEx.java
@@ -96,7 +96,7 @@ public class CommandEx implements CommandExecutor {
 	//Simple Reload Command - Might be expanded upon later.
 	private void commandReload(Player player, String[] args){
 
-		if (args.length <= 1){
+		if (args.length < 1){
 			player.sendMessage(plugin.getLang().getMessage("noperm", "warn"));
 			player.sendMessage(RELOAD);
 		}
