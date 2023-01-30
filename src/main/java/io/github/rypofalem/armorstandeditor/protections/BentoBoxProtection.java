4b96ff84e81492223a4ec7424771b23df278f289
Wolfieheart <xmakeitrain94@gmail.com>
2023-01-30 20:49:27 +0100

    [CORE] Update Bentobox Protection
    Signed-off-by: Wolfieheart <xmakeitrain94@gmail.com>

--- src/main/java/io/github/rypofalem/armorstandeditor/protections/BentoBoxProtection.java
+++ src/main/java/io/github/rypofalem/armorstandeditor/protections/BentoBoxProtection.java
@@ -3,23 +3,48 @@ package io.github.rypofalem.armorstandeditor.protections;
 import org.bukkit.Bukkit;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
+import world.bentobox.bentobox.BentoBox;
+import world.bentobox.bentobox.api.user.User;
+import world.bentobox.bentobox.database.objects.Island;
+import world.bentobox.bentobox.lists.Flags;
+import world.bentobox.bentobox.managers.AddonsManager;
+import world.bentobox.bentobox.managers.IslandsManager;
+
+import java.util.Optional;
 
 public class BentoBoxProtection implements Protection  {
 
     private final boolean bentoEnabled;
 
+
     public BentoBoxProtection() {
         bentoEnabled = Bukkit.getPluginManager().isPluginEnabled("BentoBox");
-        if(!bentoEnabled) return;
     }
 
     @Override
     public boolean checkPermission(Block block, Player player) {
-        if(!bentoEnabled || player.isOp() || player.hasPermission("asedit.ignoreProtection.bentobox")) return true;
+        BentoBox myBento = BentoBox.getInstance();
 
-        //TODO: Add API Items here
+        if(!bentoEnabled || player.isOp() ||
+                player.hasPermission("asedit.ignoreProtection.bentobox") ||
+                player.hasPermission("bentobox.admin") ||
+                myBento == null ) return true;
 
-        return true;
+        IslandsManager islandsManager = myBento.getIslandsManager();
+        AddonsManager addonsManager = myBento.getAddonsManager();
 
+        //Check first if BSkyblock is enabled or if the Player is Owner of that Island
+        if(!addonsManager.getAddonByName("BSkyblock").isPresent()) return true;
+
+        //Get the Location of the ArmorStand
+        Optional<Island> islandOptional = islandsManager.getIslandAt(block.getLocation());
+
+        //If there are no Islands Present
+        if(!islandOptional.isPresent()) return false;
+
+        //Get the Island from the Island Optional
+        Island theIsland = islandOptional.get();
+
+        return theIsland.isAllowed(User.getInstance(player), Flags.BREAK_BLOCKS);
     }
 }
