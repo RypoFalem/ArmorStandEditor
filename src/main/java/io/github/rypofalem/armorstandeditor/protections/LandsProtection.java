1134d784018e6d1db0ce06851cbafd7b51def956
Wolfieheart <xmakeitrain94@gmail.com>
2023-03-22 18:50:51 +0100

    [CORE] Hotfix to Lands. Fixes #203
    Signed-off-by: Wolfieheart <xmakeitrain94@gmail.com>

--- src/main/java/io/github/rypofalem/armorstandeditor/protections/LandsProtection.java
+++ src/main/java/io/github/rypofalem/armorstandeditor/protections/LandsProtection.java
@@ -2,38 +2,44 @@ package io.github.rypofalem.armorstandeditor.protections;
 
 import io.github.rypofalem.armorstandeditor.ArmorStandEditorPlugin;
 import me.angeschossen.lands.api.LandsIntegration;
+import me.angeschossen.lands.api.flags.type.Flags;
 import me.angeschossen.lands.api.land.Area;
 import me.angeschossen.lands.api.land.Land;
+import me.angeschossen.lands.api.land.LandWorld;
 import org.bukkit.Bukkit;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 
 public class LandsProtection implements Protection {
     private final boolean landsEnabled;
-    private LandsIntegration lands;
+    private LandsIntegration landsAPI;
 
     public LandsProtection() {
         landsEnabled = Bukkit.getPluginManager().isPluginEnabled("Lands");
 
         if (landsEnabled)
-            lands = LandsIntegration.of(ArmorStandEditorPlugin.instance());
+            landsAPI = LandsIntegration.of(ArmorStandEditorPlugin.instance());
     }
 
     @Override
     public boolean checkPermission(Block block, Player player) {
         if (!landsEnabled || player.hasPermission("asedit.ignoreProtection.lands")) return true;
 
-        //Get the land of the current block
-        Land land = lands.getLandByChunk(block.getWorld(), block.getX(), block.getZ());
+        LandWorld landWorld = landsAPI.getWorld(block.getWorld());
 
-        //Get the Area that the block is in
-        Area area = land.getArea(block.getLocation());
+        if (landWorld != null){
 
-        if(area != null)
-            //Return if Trusted in the area or not either based on BukkitPlayer
-            return area.isTrusted(player.getUniqueId());
-        else
-            return false;
+            //Get the area that the player is in
+            Area landArea = landWorld.getArea(block.getLocation());
 
+            if(landArea != null){
+                return landArea.isTrusted(player.getUniqueId());
+            } else {
+                return false;
+            }
+
+        } else {
+            return true;
+        }
     }
 }
